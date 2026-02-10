20226fe825d52ba336842dbab68bb089abff9d27
Aleksey But <aleksey.but82@yandex.ru>
2026-02-11 02:55:36 +0300

    fix style

--- src/ru/yandex/practicum/wordle/exception/game/NotOnlyRussiaLetterException.java
+++ src/ru/yandex/practicum/wordle/exception/game/NotOnlyRussiaLetterException.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.wordle.exception.game;
 
 public class NotOnlyRussiaLetterException extends GameException {
-    public NotOnlyRussiaLetterException(String s) { super(s); }
+    public NotOnlyRussiaLetterException(String s) {
+        super(s);
+    }
 }
--- src/ru/yandex/practicum/wordle/exception/game/WordNotFoundException.java
+++ src/ru/yandex/practicum/wordle/exception/game/WordNotFoundException.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.wordle.exception.game;
 
 public class WordNotFoundException extends GameException {
-    public WordNotFoundException(String s) { super(s); }
+    public WordNotFoundException(String s) {
+        super(s);
+    }
 }
